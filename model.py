aa5f2ee04a15c747dffb9b5596648258033493b9
Anika Bahl <anikabahl@anikas-air.devices.brown.edu>
2021-12-03 15:29:52 -0500

    rename model import in run.py

--- model.py
+++ model.py
@@ -15,21 +15,58 @@ class GeoLocationCNN(tf.keras.Model):
         self.learning_rate = 0.1      
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
 
-        self.architecture = [
-              Conv2D(128, 3, 1, padding="same",
-                     activation="relu"),
-              MaxPool2D(3),
-              Conv2D(128, 3, 1, padding="same",
-                     activation="relu"),
-              MaxPool2D(3),
-              Conv2D(256, 3, 1, padding="same",
-                     activation="relu"),
-              MaxPool2D(3),
-              Dropout(0.4),
-              Dense(32),
-              Flatten(),
-              Dense(15, activation='softmax')
+        self.vgg16 = [
+            # Block 1
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv1"),
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv2"),
+            MaxPool2D(2, name="block1_pool"),
+            # Block 2
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv1"),
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv2"),
+            MaxPool2D(2, name="block2_pool"),
+            # Block 3
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv1"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv2"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv3"),
+            MaxPool2D(2, name="block3_pool"),
+            # Block 4
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv3"),
+            MaxPool2D(2, name="block4_pool"),
+            # Block 5
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv3"),
+            MaxPool2D(2, name="block5_pool")
         ]
+
+        for layer in self.vgg16:
+               layer.trainable = False
+
+        self.head = [ 
+               Flatten(),
+               Dropout(rate=0.4),
+               Dense(512, activation="relu"),
+               Dense(256, activation="relu"),
+               Dense(15, activation="softmax")
+        ]
+
+        self.vgg16 = tf.keras.Sequential(self.vgg16, name="vgg_base")
+        self.head = tf.keras.Sequential(self.head, name="vgg_head")
 
     def call(self, x):
         """ Passes input image through the network. """
