7935e2299462fdc7f69c6b6ac64e7f955cbef970
Ilya Bodo <58499382+ilyabodo@users.noreply.github.com>
2021-12-09 17:06:43 -0500

    Updated selenium interface

--- Geoguessr/main.py
+++ Geoguessr/main.py
@@ -1,21 +1,45 @@
+import os
+import sys
+import argparse
 from geoguessr_getter import GeoGetter
 
-def main():
-    browser = input("Which browser would you like to use? (Chrome/Firefox)")
-    maap = input("Which map? (Europe)")
+def main(ARGS):
+    browser = ARGS.browser
+    maap = ARGS.map
     gg = GeoGetter(browser, maap)
 
-    t = input("enter when ready")
-    gg.get_coordinates()
-    #print(r)
+    for i in range(5):
+        print("Round: ", i+1)
+        t = input("Please hit enter when ready!")
+        lat, lon = gg.get_coordinates()
+
+    input("Game complete. Please hit enter to quit!")
     gg.quit()
 
+def parse_args():
+    """ Perform command-line argument parsing. """
 
+    parser = argparse.ArgumentParser(
+        description="Let's guess some locations!",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--browser',
+        required=True,
+        choices=['chrome', 'firefox'],
+        help='''Which web browser to to used.''')
+    parser.add_argument(
+        '--map',
+        required=True,
+        choices=['europe'],
+        help='''Which map to be played. (Only Europe map supported currently)''')
+    parser.add_argument(
+        '--visualize',
+        action='store_true',
+        help='''Turns on/off visual results.''')
 
-
-
-
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    main()
+    ARGS = parse_args()
+    main(ARGS)
