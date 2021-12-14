ace2e655b0840c6b0697b142b384c48b0a456fe1
Aryan Srivastava <aryan@Aryans-MacBook-Air.local>
2021-12-14 04:17:11 -0500

    finetuning

--- tensorboard_utils.py
+++ tensorboard_utils.py
@@ -195,13 +195,9 @@ class CustomModelSaver(tf.keras.callbacks.Callback):
             save_name = "weights.e{0:03d}-acc{1:.4f}.h5".format(
                 epoch, cur_acc)
 
-            if self.task == '1':
-                self.model.save_weights(
-                    self.checkpoint_dir + os.sep + "your." + save_name)
-            else:
-                # Only save weights of classification head of VGGModel
-                self.model.head.save_weights(
-                    self.checkpoint_dir + os.sep + "vgg." + save_name)
+
+            self.model.save_weights(
+                self.checkpoint_dir + os.sep + "your." + save_name)
 
             # Ensure max_num_weights is not exceeded by removing
             # minimum weight
